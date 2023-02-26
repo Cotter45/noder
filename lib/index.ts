f56436f5e31adc50269452a911aa5c1dc113ba9f
Sean <sd.cotter45@gmail.com>
2023-02-26 14:40:00 -0500

    trying to include types in npm

--- lib/index.ts
+++ lib/index.ts
@@ -2,5 +2,7 @@ import { Router } from './router';
 import { Route } from './route';
 import { Server } from './server';
 import { NotFoundError, ServerError } from './errors';
+import type { IRequest, IResponse, ICtx } from './types';
 
 export { Router, Route, Server, NotFoundError, ServerError };
+export type { IRequest, IResponse, ICtx };
