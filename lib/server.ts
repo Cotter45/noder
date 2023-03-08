311e6be6f5af5ab13e96f927a4b1b0bfab453133
Sean <sd.cotter45@gmail.com>
2023-03-07 22:01:23 -0500

    added more mime types and added check for the file

--- lib/server.ts
+++ lib/server.ts
@@ -91,8 +91,7 @@ export class Server {
     res: http.ServerResponse,
   ) => {
     const pathname = req.url;
-    const filePath =
-      pathname === '/' || pathname === '' ? '/index.html' : pathname;
+    const filePath = !pathname || pathname === '/' ? '/index.html' : pathname;
     const extname = filePath
       ? String(path.extname(filePath)).toLowerCase()
       : '.html';
@@ -103,27 +102,100 @@ export class Server {
       res.end();
       return;
     }
+
     const mimeTypes: { [key: string]: string } = {
-      '.html': 'text/html',
-      '.js': 'text/javascript',
-      '.css': 'text/css',
-      '.json': 'application/json',
-      '.png': 'image/png',
-      '.jpg': 'image/jpg',
-      '.gif': 'image/gif',
-      '.svg': 'image/svg+xml',
-      '.wav': 'audio/wav',
-      '.mp4': 'video/mp4',
-      '.woff': 'application/font-woff',
-      '.ttf': 'application/font-ttf',
+      '.aac': 'audio/aac',
+      '.abw': 'application/x-abiword',
+      '.arc': 'application/x-freearc',
+      '.avi': 'video/x-msvideo',
+      '.avif': 'image/avif',
+      '.azw': 'application/vnd.amazon.ebook',
+      '.bin': 'application/octet-stream',
+      '.bmp': 'image/bmp',
+      '.bz': 'application/x-bzip',
+      '.bz2': 'application/x-bzip2',
+      '.cda': 'application/x-cdf',
+      '.csh': 'application/x-csh',
+      '.csv': 'text/csv',
+      '.doc': 'application/msword',
+      '.docx':
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
       '.eot': 'application/vnd.ms-fontobject',
-      '.otf': 'application/font-otf',
+      '.epub': 'application/epub+zip',
+      '.gz': 'application/gzip',
+      '.gif': 'image/gif',
+      '.htm': 'text/html',
+      '.html': 'text/html',
+      '.ico': 'image/vnd.microsoft.icon',
+      '.ics': 'text/calendar',
+      '.jar': 'application/java-archive',
+      '.jpeg': 'image/jpeg',
+      '.jpg': 'image/jpeg',
+      '.js': 'text/javascript',
+      '.json': 'application/json',
+      '.jsonld': 'application/ld+json',
+      '.mid': 'audio/midi audio/x-midi',
+      '.midi': 'audio/midi audio/x-midi',
+      '.mjs': 'text/javascript',
+      '.mp3': 'audio/mpeg',
+      '.mpeg': 'video/mpeg',
+      '.mp4': 'video/mp4',
+      '.mpkg': 'application/vnd.apple.installer+xml',
+      '.odp': 'application/vnd.oasis.opendocument.presentation',
+      '.ods': 'application/vnd.oasis.opendocument.spreadsheet',
+      '.odt': 'application/vnd.oasis.opendocument.text',
+      '.oga': 'audio/ogg',
+      '.ogv': 'video/ogg',
+      '.ogx': 'application/ogg',
+      '.opus': 'audio/opus',
+      '.otf': 'font/otf',
+      '.png': 'image/png',
+      '.pdf': 'application/pdf',
+      '.php': 'application/x-httpd-php',
+      '.ppt': 'application/vnd.ms-powerpoint',
+      '.pptx':
+        'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+      '.rar': 'application/vnd.rar',
+      '.rtf': 'application/rtf',
+      '.sh': 'application/x-sh',
+      '.svg': 'image/svg+xml',
+      '.tar': 'application/x-tar',
+      '.tif': 'image/tiff',
+      '.tiff': 'image/tiff',
+      '.ts': 'video/mp2t',
+      '.ttf': 'font/ttf',
+      '.txt': 'text/plain',
+      '.vsd': 'application/vnd.visio',
+      '.wav': 'audio/wav',
+      '.weba': 'audio/webm',
+      '.webm': 'video/webm',
+      '.webp': 'image/webp',
+      '.woff': 'font/woff',
+      '.woff2': 'font/woff2',
+      '.xhtml': 'application/xhtml+xml',
+      '.xls': 'application/vnd.ms-excel',
+      '.xlsx':
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      '.xml': 'application/xml',
+      '.xul': 'application/vnd.mozilla.xul+xml',
+      '.zip': 'application/zip',
+      '.3gp': 'video/3gpp',
+      '.3g2': 'video/3gpp2',
+      '.7z': 'application/x-7z-compressed',
+      '.css': 'text/css',
       '.wasm': 'application/wasm',
+      '.webmanifest': 'application/manifest+json',
     };
 
     const contentType = mimeTypes[extname] || 'application/octet-stream';
-
     const file = this.staticFileMap[filePath];
+
+    if (!file) {
+      res.writeHead(404);
+      res.end(`Sorry, check with the site admin for error: ${res.statusCode}`);
+      res.end();
+      return;
+    }
 
     fs.readFile(file, (error, content) => {
       if (error) {
